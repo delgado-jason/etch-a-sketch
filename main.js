34a2739413dbd6e649e9a64c230b0aa6c7c5bd7b
grandschema <blackhat23507@gmail.com>
2024-03-16 00:22:01 -0400

    Finish v1.0 of the program
    This program is fully functional. Other things to consider:
    Better design.
    Start out with a grid.
    Ability to erase. Not just reset.

--- main.js
+++ main.js
@@ -1,13 +1,61 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.square-container');
+const resetButton = document.querySelector("#reset");
+const squareSetBtn = document.querySelector("#grid-size");
+
+
+const gridWidth = 700;
 
 
 
-// Create 16 squares
-for(let i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    div.setAttribute("class", "square");
-    div.addEventListener("mouseenter", (event) => {
-        div.style.backgroundColor = "blue";
-    })
-    container.appendChild(div);
+// Initialize a 5x5 grid
+
+
+// Create grid
+function buildGrid(squares){
+    const squareWidth = gridWidth / squares;
+    const squareHeight = gridWidth / squares;
+    for(let i = 0; i < (squares * squares); i++){
+        const div = document.createElement("div");
+        div.setAttribute("class", "square");
+        div.style.width = `${squareWidth}px`;
+        div.style.height = `${squareHeight}px`;
+        div.addEventListener("mouseenter", (event) => {
+            div.style.backgroundColor = "#3794DB";
+        })
+        container.appendChild(div);
+    }
+}
+
+// Reset the squares
+resetButton.addEventListener('click', (event) => {
+    reset();
+})
+
+function reset(){
+    for(let i = 0; i < container.children.length; i++){
+        container.children[i].style.backgroundColor = "red";
+    }
+}
+
+// Get Number of squares from user
+squareSetBtn.addEventListener('click', (event) => {
+    resetGrid();
+    let squares = getSquares();
+    buildGrid(squares);
+})
+
+function getSquares(){
+    let squares = parseInt(prompt("Enter the amount of squares per side: "));
+    if(squares <= 40){
+        return squares;
+    } else{
+        alert("Can't be greater than 40!");
+    }
+}
+
+// Reset the grid
+function resetGrid(){
+    while(container.children.length > 0){
+        container.children[0].remove();
+    }
 }
